4ddf573368db2657aa1ad7bfb55af025a941da49
ggwalplay <204519158+ggwalplay@users.noreply.github.com>
2026-07-29 13:14:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.presences = True  # requis pour lire le statut personnalisé (commande &soutien)
 
 bot = commands.Bot(command_prefix="&", intents=intents)
 
